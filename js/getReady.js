16c58587a3009b9b46d2cc0bf3dfc66e51598a07
PildoritaGalactica <skualed@gmail.com>
2021-07-12 17:39:46 +0200

    getReady.js is now loadGetReadyTemplate.js

--- js/getReady.js
+++
@@ -1,6 +0,0 @@
-const gameBoard = document.querySelector("#gameboard");
-const template = `
-  <div id="get-ready">
-    <h1 class="get-ready__title">Get ready...</h1>
-  </div>
-`;
---
+++ js/loadGetReadyTemplate.js
@@ -0,0 +1,8 @@
+function loadGetReadyTemplate() {
+  const gameBoard = document.querySelector("#gameboard");
+  const template = `
+  <div id="get-ready">
+    <h1 class="get-ready__title">Get ready...</h1>
+  </div>
+`;
+}
